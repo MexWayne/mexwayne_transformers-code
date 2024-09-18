c05fbdf26ceca81ed656ce090b3c5053fadc506f
mex <mex_wayne@163.com>
2024-09-18 09:21:22 +0800

    [NEW][dataset]: 1 add the collater learning code

--- 01-Getting_Started/05-datasets/datasets_demo.py
+++ 01-Getting_Started/05-datasets/datasets_demo.py
@@ -4,6 +4,7 @@
 
 from datasets import *
 from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
 
 # add a prefix for every title
 def add_prefix(example):
@@ -18,89 +19,96 @@ def preprocess_function(example, tokenizer = tokenizer):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
+def process_function(examples):
+    tokenized_examples = tokenizer(examples["review"], max_length=128, truncation=True)
+    tokenized_examples["labels"] = examples["label"]
+    return tokenized_examples
+
+
 if __name__ == "__main__":
 
 
-    ####################################################### basic operation
-    ## add a dataset
-    #data_set = load_dataset("madao33/new-title-chinese")
-    #print(data_set)
-    #print("------------------------------")
-    #print("train[0]:")
-    #print(data_set["train"][0])
-    #print("------------------------------")
-    #print("train[:2]:")
-    #print(data_set["train"][:2])
-    #print("------------------------------")
-    #print("train[\"tile\"][:5]:")
-    #print(data_set["train"]["title"][:5])
-    #print("------------------------------")
+    ###################################################### basic operation
+    # add a dataset
+    data_set = load_dataset("madao33/new-title-chinese")
+    print(data_set)
+    print("------------------------------")
+    print("train[0]:")
+    print(data_set["train"][0])
+    print("------------------------------")
+    print("train[:2]:")
+    print(data_set["train"][:2])
+    print("------------------------------")
+    print("train[\"tile\"][:5]:")
+    print(data_set["train"]["title"][:5])
+    print("------------------------------")
 
 
 
 
-    ####################################################### load specific part or implement
-    ## add specific task
-    #boolq_dataset = load_dataset("super_glue", "boolq",trust_remote_code=True)
-    #print(boolq_dataset)
+    ###################################################### load specific part or implement
+    # add specific task
+    boolq_dataset = load_dataset("super_glue", "boolq",trust_remote_code=True)
+    print(boolq_dataset)
 
 
-    #dataset = load_dataset("madao33/new-title-chinese", split="train")
-    #print("train:") 
-    #print(dataset)
+    dataset = load_dataset("madao33/new-title-chinese", split="train")
+    print("train:") 
+    print(dataset)
 
-    #dataset = load_dataset("madao33/new-title-chinese", split="train[10:100]")
-    #print("train 10:100:") 
-    #print(dataset)
+    dataset = load_dataset("madao33/new-title-chinese", split="train[10:100]")
+    print("train 10:100:") 
+    print(dataset)
     
-    #dataset = load_dataset("madao33/new-title-chinese", split="train[10%:50%]")
-    #print("train 10%:100%:") 
-    #print(dataset)
+    dataset = load_dataset("madao33/new-title-chinese", split="train[10%:50%]")
+    print("train 10%:100%:") 
+    print(dataset)
 
-    #dataset = load_dataset("madao33/new-title-chinese", split=["train[:40%]", "train[40%:]"])
-    #print("train 40% and 60%:") 
-    #print(dataset)
+    dataset = load_dataset("madao33/new-title-chinese", split=["train[:40%]", "train[40%:]"])
+    print("train 40% and 60%:") 
+    print(dataset)
 
 
-    ######################################################### split
-    #datasets = load_dataset("madao33/new-title-chinese")
-    #print("origin train datasets:")
-    #print(datasets["train"])
-    #print("-----------------")
-    #print("make train set as test 0.1:")
-    #dataset = datasets["train"]
-    #print(dataset.train_test_split(test_size=0.1))
-    #print("-----------------")
-    #print("stratify:")
-    #boolq_dataset = load_dataset("super_glue", "boolq",trust_remote_code=True)
-    #dataset = boolq_dataset["train"]
-    #print(dataset.train_test_split(test_size=0.1, stratify_by_column="label"))     # 分类数据集可以按照比例划分
-    #print("-----------------")
+    ######################################################## split
+    datasets = load_dataset("madao33/new-title-chinese")
+    print("origin train datasets:")
+    print(datasets["train"])
+    print("-----------------")
+    print("make train set as test 0.1:")
+    dataset = datasets["train"]
+    print(dataset.train_test_split(test_size=0.1))
+    print("-----------------")
+    print("stratify:")
+    boolq_dataset = load_dataset("super_glue", "boolq",trust_remote_code=True)
+    dataset = boolq_dataset["train"]
+    print(dataset.train_test_split(test_size=0.1, stratify_by_column="label"))     # 分类数据集可以按照比例划分
+    print("-----------------")
 
-    ########################################### select and filter
-    #datasets = load_dataset("madao33/new-title-chinese")
-    ## 选取
-    #filter_res = datasets["train"].select([0, 1])
-    #print("select:")
-    #print(filter_res["title"][:5])
-    ## 过滤
-    #filter_dataset = datasets["train"].filter(lambda example: "中国" in example["title"])
-    #print("filter:")
-    #print(filter_dataset["title"][:5])
+    ########################################## select and filter
+    datasets = load_dataset("madao33/new-title-chinese")
+    # 选取
+    filter_res = datasets["train"].select([0, 1])
+    print("select:")
+    print(filter_res["title"][:5])
+    # 过滤
+    filter_dataset = datasets["train"].filter(lambda example: "中国" in example["title"])
+    print("filter:")
+    print(filter_dataset["title"][:5])
     
     
-    ######################################### mapping
-    #datasets = load_dataset("madao33/new-title-chinese")
-    #prefix_dataset = datasets.map(add_prefix)
-    #print(prefix_dataset["train"][:10]["title"])
+    ######################################## mapping
+    datasets = load_dataset("madao33/new-title-chinese")
+    prefix_dataset = datasets.map(add_prefix)
+    print(prefix_dataset["train"][:10]["title"])
 
-    #processed_datasets = datasets.map(preprocess_function)
-    #print("train:")
-    #print(processed_datasets["train"][:5])
-    #print("validation:")
-    #print(processed_datasets["validation"][:5])
+    processed_datasets = datasets.map(preprocess_function)
+    print("train:")
+    print(processed_datasets["t
+     rain"][:5])
+    print("validation:")
+    print(processed_datasets["validation"][:5])
 
-    ######################################## load and save
+    ####################################### load and save
     datasets = load_dataset("madao33/new-title-chinese")
     processed_datasets = datasets.map(preprocess_function)
     print("from web:") 
@@ -111,3 +119,24 @@ if __name__ == "__main__":
     print("from local:") 
     print(processed_datasets["validation"][:2])
 
+
+    ####################################### collator
+    local_dataset = load_dataset("csv", data_files="/home/mex/Desktop/learn_transformer/mexwayne_transformers_NLP/01-Getting_Started/05-datasets/ChnSentiCorp_htl_all.csv", split="train")
+    local_dataset = local_dataset.filter(lambda x: x["review"] is not None)
+    print(local_dataset)
+
+    tokenized_dataset = local_dataset.map(process_function, batched=True, remove_columns=local_dataset.column_names)
+    print(tokenized_dataset[:3])
+
+    collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    from torch.utils.data import DataLoader
+    dl = DataLoader(tokenized_dataset, batch_size = 4, collate_fn = collator, shuffle = True)
+    num = 0
+    for batch in dl:
+        print(batch["input_ids"].size())
+        num += 1
+        if num > 10:
+            break
+
+    print("finish!")
